da71243607e45f802438328bdb5b41cefb2acf6e
SNQ-2001 <info@taishin-miyamoto.com>
2023-06-16 15:17:17 +0900

    feat: HapticsButtonSwiftUIを作成

---
+++ Sources/Haptics/Haptics.swift
@@ -0,0 +1,42 @@
+//
+//  Haptics.swift
+//  
+//
+//  Created by 宮本大新 on 2023/06/16.
+//
+
+import class UIKit.UIImpactFeedbackGenerator
+import class UIKit.UINotificationFeedbackGenerator
+import class UIKit.UISelectionFeedbackGenerator
+
+func playFeedbackHaptic(_ style: UIImpactFeedbackGenerator.FeedbackStyle) {
+    let generator = UIImpactFeedbackGenerator(style: style)
+    generator.impactOccurred()
+}
+
+func playNotificationHaptic(_ type: UINotificationFeedbackGenerator.FeedbackType) {
+    let generator = UINotificationFeedbackGenerator()
+    generator.notificationOccurred(type)
+}
+
+func playSelectionHaptic() {
+    let generator = UISelectionFeedbackGenerator()
+    generator.selectionChanged()
+}
+
+public enum Haptics {
+    case impact(_ style: UIImpactFeedbackGenerator.FeedbackStyle)
+    case notification(_ type: UINotificationFeedbackGenerator.FeedbackType)
+    case selection
+    
+    public func play() {
+        switch self {
+        case let .impact(style):
+            playFeedbackHaptic(style)
+        case let .notification(type):
+            playNotificationHaptic(type)
+        case .selection:
+            playSelectionHaptic()
+        }
+    }
+}
--- Sources/HapticsButtonSwiftUI/HapticsButtonSwiftUI.swift
+++ Sources/HapticsButtonSwiftUI/HapticsButtonSwiftUI.swift
@@ -1,2 +1,28 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+import SwiftUI
+import Haptics
+
+struct Button<T: View>: View {
+    private let haptics: Haptics
+
+    private let action: () -> Void
+    
+    private let label: () -> T
+    
+    init(haptics: Haptics, action: @escaping () -> Void, label: @escaping () -> T) {
+        self.haptics = haptics
+        self.action = action
+        self.label = label
+    }
+
+    var body: some View {
+        SwiftUI.Button {
+            haptics.play()
+            action()
+        } label: {
+            label()
+        }
+    }
+}
