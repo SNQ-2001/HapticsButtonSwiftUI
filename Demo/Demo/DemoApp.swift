5800b1931c56196c0aae5d9aa6c857223254d078
SNQ-2001 <info@taishin-miyamoto.com>
2023-06-16 15:26:25 +0900

    feat: デモを追加

---
+++ Demo/Demo/DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoApp.swift
+//  Demo
+//
+//  Created by 宮本大新 on 2023/06/16.
+//
+
+import SwiftUI
+
+@main
+struct DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
