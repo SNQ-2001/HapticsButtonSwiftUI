05ebb22662d3a61ea739cb30494f3409f0f306a9
SNQ-2001 <info@taishin-miyamoto.com>
2023-10-13 01:09:28 +0900

    feat: Buttonにruleを指定できるように変更

--- Sources/HapticsButtonSwiftUI/HapticsButtonSwiftUI.swift
+++ Sources/HapticsButtonSwiftUI/HapticsButtonSwiftUI.swift
@@ -1,24 +1,28 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 
+
 import SwiftUI
 import Haptics
 
 public struct Button<T: View>: View {
+    private let role: ButtonRole?
+
     private let haptics: Haptics
 
     private let action: () -> Void
     
     private let label: () -> T
     
-    public init(haptics: Haptics, action: @escaping () -> Void, label: @escaping () -> T) {
+    public init(role: ButtonRole? = nil, haptics: Haptics, action: @escaping () -> Void, label: @escaping () -> T) {
+        self.role = role
         self.haptics = haptics
         self.action = action
         self.label = label
     }
 
     public var body: some View {
-        SwiftUI.Button {
+        SwiftUI.Button(role: role) {
             haptics.play()
             action()
         } label: {
